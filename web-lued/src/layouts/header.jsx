99ba297dd39627a165cd92f8bf71db05c4e75e2f
Lued <luedvaldes23@gmail.com>
2024-06-17 10:34:49 -0500

    botones con materia ui

--- web-lued/src/layouts/header.jsx
+++ web-lued/src/layouts/header.jsx
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import { SearchContext } from "../components/context/SearchContext";
 import './header.scss'
 import logo from '../layouts/Lued_Logo-.png'
+import { Button } from '@mui/material';
 
 const Header = () => {
   
@@ -30,37 +31,97 @@ const Header = () => {
       </div>
         
         <ul className="header__nav-list">
-          <li className="header__list-item">
-            <NavLink
-                to="/"
-                className={({ isActive })=>linkIsActive(isActive)}
+        <li className="header__list-item">
+        <NavLink
+          to="/"
+          className={({ isActive }) => linkIsActive(isActive)}
+        >
+          {({ isActive }) => (
+            <Button
+              variant="contained"
+              sx={{
+                width: '100px',
+                backgroundColor: 'white',
+                color: isActive ? 'pink' : 'pink',
+                '&:hover': {
+                  backgroundColor: '#f0f0f0',
+                },
+                border: isActive ? '2px solid pink' : '2px solid transparent',
+              }}
             >
-                Home
-            </NavLink>
-           </li>
-          <li className="header__list-item">
-            <NavLink
-                to="/Menu"
-                className={({ isActive })=>linkIsActive(isActive)}
+              Home
+            </Button>
+          )}
+        </NavLink>
+      </li>
+      <li className="header__list-item">
+        <NavLink
+          to="/Menu"
+          className={({ isActive }) => linkIsActive(isActive)}
+        >
+          {({ isActive }) => (
+            <Button
+              variant="contained"
+              sx={{
+                width: '100px',
+                backgroundColor: 'white',
+                color: isActive ? 'pink' : 'pink',
+                '&:hover': {
+                  backgroundColor: '#f0f0f0',
+                },
+                border: isActive ? '2px solid pink' : '2px solid transparent',
+              }}
             >
-               Menu
-            </NavLink>
-            </li>
+              Menu
+            </Button>
+          )}
+        </NavLink>
+      </li>
             <li className="header__list-item">
             <NavLink
                 to="/Pasteleria"
                 className={({ isActive })=>linkIsActive(isActive)}
             >
+             {({ isActive }) => (
+            <Button
+              variant="contained"
+              sx={{
+                width: '100px',
+                backgroundColor: 'white',
+                color: isActive ? 'pink' : 'pink',
+                '&:hover': {
+                  backgroundColor: '#f0f0f0',
+                },
+                border: isActive ? '2px solid pink' : '2px solid transparent',
+              }}
+            >
               Pasteleria
-            </NavLink>
+            </Button>
+          )}
+        </NavLink>
             </li>
             <li className="header__list-item">
             <NavLink
                 to="/Panaderia"
                 className={({ isActive })=>linkIsActive(isActive)}
             >
+              {({ isActive }) => (
+            <Button
+              variant="contained"
+              sx={{
+                width: '100px',
+                backgroundColor: 'white',
+                color: isActive ? 'pink' : 'pink',
+                '&:hover': {
+                  backgroundColor: '#f0f0f0',
+                },
+                border: isActive ? '2px solid pink' : '2px solid transparent',
+              }}
+            >
               Panaderia
-            </NavLink>
+            </Button>
+          )}
+        </NavLink>
             </li>
         </ul>
       </nav>
